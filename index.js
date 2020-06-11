111ad30a9e99cd13724cc62015e42968a83c7f50
Shivam Mehta <mehtashivam91@gmail.com>
2020-06-11 13:29:31 +0100

    admin console

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-var app = require('express')();
-var http = require('http').createServer(app);
+const app = require("express")();
+const http = require("http").createServer(app);
+const expressLess = require("express-less");
+app.use("/less-css", expressLess(__dirname + "/less"));
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+app.get("/admin", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
 http.listen(3000, () => {
-  console.log('listening on *:3000');
+  console.log("👂 on *:3000 🚀🚀🚀🚀🚀🚀🚀");
 });
