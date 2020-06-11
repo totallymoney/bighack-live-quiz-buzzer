1a6c106571e3e8a53d8d842ab72efac6c2ed5c14
Shivam Mehta <mehtashivam91@gmail.com>
2020-06-11 13:47:50 +0100

    add sockets

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const app = require("express")();
 const http = require("http").createServer(app);
 const expressLess = require("express-less");
+const io = require("socket.io")(http);
 app.use("/less-css", expressLess(__dirname + "/less"));
 
 app.get("/admin", (req, res) => {
+  res.sendFile(__dirname + "/admin.html");
+});
+
+app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
+io.on("connection", (socket) => {
+  console.log("a played connected 🤽‍♀️");
+});
+
 http.listen(3000, () => {
-  console.log("👂 on *:3000 🚀🚀🚀🚀🚀🚀🚀");
+  console.log("👂 on *:3000 🚀 🚀 🚀");
 });
